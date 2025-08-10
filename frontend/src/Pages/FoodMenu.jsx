f4368f765a42e1f57edac8b56088ba33b401adf5
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-08-10 18:27:20 +0530

    fix: fix meal fetchng not done

--- frontend/src/Pages/FoodMenu.jsx
+++ frontend/src/Pages/FoodMenu.jsx
@@ -1,11 +1,20 @@
 import HeroAllPages from '@/components/HeroAllPages'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { HeroBanner } from '@/images/images'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchMeals } from '@/redux/slices/mealSlice'
 
 const FoodMenu = () => {
+  const { meals, isLoading, error } = useSelector((state) => state.meals);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMeals());
+  }, [dispatch]);
+  console.log(meals, isLoading, error);
   return (
     <div>
-            <HeroAllPages title="Food Menu" description="Explore our delicious food options" image={HeroBanner} />
+      <HeroAllPages title="Food Menu" description="Explore our delicious food options" image={HeroBanner} />
+
     </div>
   )
 }
